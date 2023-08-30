8cffa12536d6a3199f2876b3c96787266d43b265
Mike Mitterer <office@mikemitterer.at>
2023-08-30 15:52:51 +0200

    Alle Versionen nach oben gezogen

--- babel.config.js
+++ babel.config.js
@@ -24,6 +24,7 @@ module.exports = {
         '@babel/typescript'
     ],
     plugins: [
+        // yarn add -D "@babel/plugin-proposal-class-properties" "@babel/plugin-proposal-object-rest-spread" "@babel/plugin-transform-runtime" "@babel/plugin-proposal-object-rest-spread" "@babel/plugin-proposal-nullish-coalescing-operator" "@babel/plugin-proposal-optional-chaining" "babel-plugin-transform-inline-environment-variables"
         '@babel/proposal-class-properties',
         '@babel/proposal-object-rest-spread',
         '@babel/plugin-proposal-nullish-coalescing-operator',
